e0c7993d04a109c2703be3a9c4509b627091adb9
pratik28 <pratik28@users.noreply.github.com>
2023-08-26 00:22:21 +0530

    Update Regression-ML-WebAPI.r
    Added missing quote

--- Regression-ML-WebAPI.r
+++ Regression-ML-WebAPI.r
@@ -2,9 +2,10 @@
 library(httr)
 library(xml2) 
 library(readr) 
-library(rjson) 
+#library(rjson) 
+library(jsonlite) 
 library(tidyverse)
-library(caret)
+#library(caret)
 library(leaps)
 library(caTools) 
 library(quickpsy) 
@@ -19,46 +20,45 @@ r <- GET(
     config = authenticate("pratik", "pratik"), 
     encode = "json" 
   )
-print(r) 
+print(r)  
 
 #Construct a data object to hold the request body (i.e., the container that needs to be created)  
 
-my_data_obj = '{   	"container_name":"HCC_Data"  ,  	"container_type":"COLLECTION" ,    	"rowkey": false,    "columns": [  
-{ { "name": "Gender"  ,"type": "FLOAT" },{ "name": "Symptoms"  ,"type": "FLOAT" },  { "name": "Alcohol"  ,"type": "FLOAT" },
-  { "name": "Hepatitis B Surface Antigen"  ,"type": "FLOAT" },{ "name": "Hepatitis B e Antigen"  ,"type": "FLOAT" },{ "name": "Hepatitis B Core Antibody"  ,"type": "FLOAT" },{ "name": "Hepatitis C Virus Antibody"  ,"type": "FLOAT" },
-  { "name": "Smoking"  ,"type": "FLOAT" },{ "name": "Diabetes"  ,"type": "FLOAT" }, { "name": "Obesity"  ,"type": "FLOAT" },{ "name": "Hemochromatosis"  ,"type": "FLOAT" }, { "name": "Arterial Hypertension"  ,"type": "FLOAT" },{ "name": "Chronic Renal Insufficiency"  ,"type": "FLOAT" },{ "name": "Nonalcoholic Steatohepatitis"  ,"type": "FLOAT" }, { "name": "Esophageal Varices"  ,"type": "FLOAT" },{ "name": "Splenomegaly"  ,"type": "FLOAT" },{ "name": "Liver Metastasis"  ,"type": "FLOAT" },  { "name": "Age at diagnosis"  ,"type": "FLOAT" },{ "name": "Packs of cigarets per year"  ,"type": "FLOAT" },{ "name": "Performance Status"  ,"type": "FLOAT" },
-  { "name": "Encefalopathy	degree"  ,"type": "FLOAT" },{ "name": "Ascites degree"  ,"type": "FLOAT" }, { "name": "International Normalised Ratio"  ,"type": "FLOAT" },{ "name": "Alpha-Fetoprotein"  ,"type": "FLOAT" },
-  { "name": "Haemoglobin"  ,"type": "FLOAT" },{ "name": "Mean Corpuscular Volume"  ,"type": "FLOAT" }, { "name": "Leukocytes"  ,"type": "FLOAT" },{ "name": "Platelets"  ,"type": "FLOAT" },
-  { "name": "Albumin"  ,"type": "FLOAT" },{ "name": "Total Bilirubin"  ,"type": "FLOAT" }, { "name": "Alanine transaminase"  ,"type": "FLOAT" },{ "name": "Alkaline phosphatase"  ,"type": "FLOAT" }, { "name": "Total Proteins"  ,"type": "FLOAT" },{ "name": "Creatinine"  ,"type": "FLOAT" }, { "name": "Number of Nodules"  ,"type": "FLOAT" },{ "name": "Direct Bilirubin"  ,"type": "FLOAT" }, { "name": "Iron"  ,"type": "FLOAT" },{ "name": "Oxygen Saturation"  ,"type": "FLOAT" }, { "name": "Ferritin"  ,"type": "FLOAT" },{ "name": "Class"  ,"type": "FLOAT" }, { "name": "Alive"  ,"type": "FLOAT" } }  ] 
-  }
+my_data_obj = '{   	"container_name":"HCC_Data4"  ,  	"container_type":"COLLECTION" ,    	"rowkey": false,    "columns": [  { "name": "Gender"  ,"type": "FLOAT" },{ "name": "Symptoms"  ,"type": "FLOAT" },  { "name": "Alcohol"  ,"type": "FLOAT" }, { "name": "Hepatitis_B_Surface_Antigen"  ,"type": "FLOAT" },{ "name": "Hepatitis_B_e_Antigen"  ,"type": "FLOAT" },{ "name": "Hepatitis_B_Core_Antibody"  ,"type": "FLOAT" },{ "name": "Hepatitis_C_Virus_Antibody"  ,"type": "FLOAT" }, { "name": "Smoking"  ,"type": "FLOAT" },{ "name": "Diabetes"  ,"type": "FLOAT" }, { "name": "Obesity"  ,"type": "FLOAT" },{ "name": "Hemochromatosis"  ,"type": "FLOAT" }, { "name": "Arterial_Hypertension"  ,"type": "FLOAT" },{ "name": "Chronic_Renal_Insufficiency"  ,"type": "FLOAT" },{ "name": "Nonalcoholic_Steatohepatitis"  ,"type": "FLOAT" }, { "name": "Esophageal_Varices"  ,"type": "FLOAT" },{ "name": "Splenomegaly"  ,"type": "FLOAT" },{ "name": "Liver_Metastasis"  ,"type": "FLOAT" },  { "name": "Age_at_diagnosis"  ,"type": "FLOAT" },{ "name": "Packs_of_cigarets_per_year"  ,"type": "FLOAT" },{ "name": "Performance_Status" ,"type": "FLOAT" },   { "name": "Encefalopathy_degree"  ,"type": "FLOAT" },{ "name": "Ascites_degree"  ,"type": "FLOAT" }, { "name": "International_Normalised_Ratio"  ,"type": "FLOAT" },{ "name": "Alpha_Fetoprotein"  ,"type": "FLOAT" },   {"name": "Haemoglobin"  ,"type": "FLOAT" },{ "name": "Mean_Corpuscular_Volume"  ,"type": "FLOAT" }, { "name": "Leukocytes"  ,"type": "FLOAT" },{ "name": "Platelets"  ,"type": "FLOAT" },   { "name": "Albumin"  ,"type": "FLOAT" },{ "name": "Total_Bilirubin"  ,"type": "FLOAT" }, { "name": "Alanine_transaminase"  ,"type": "FLOAT" },{ "name": "Alkaline_phosphatase"  ,"type": "FLOAT" }, { "name": "Total_Proteins"  ,"type": "FLOAT" },{ "name": "Creatinine"  ,"type": "FLOAT" }, { "name": "Number_of_Nodules"  ,"type": "FLOAT" },{ "name": "Direct_Bilirubin"  ,"type": "FLOAT" }, { "name": "Iron"  ,"type": "FLOAT" },{ "name": "Oxygen_Saturation"  ,"type": "FLOAT" }, { "name": "Ferritin"  ,"type": "FLOAT" },{ "name": "Class"  ,"type": "FLOAT" }, { "name": "Alive"  ,"type": "FLOAT" }   ]    } ' 
   
 #Set up the GridDB WebAPI URL
-container_url = "https://cloud1.griddb.com/trial1602/griddb/v2/gs_clustertrial1602/dbs/r_blog/containers/"
+container_url = "https://cloud1.griddb.com/trial1602/griddb/v2/gs_clustertrial1602/dbs/r_blog/containers"
 #Lets now invoke the POST request via GridDB WebAPI with the headers and the request body 
  r <- POST(container_url, 
        add_headers("Content-Type" = "application/json; charset=UTF-8" ) ,      
        config = authenticate("pratik", "pratik"), 
        encode = "json", 
        body= my_data_obj) 
-	  
+
+print("After container create")	  
 # Populate the container with rows from JSON or CSV 
-hcc_data_JSON <- fromJSON(hcc_data.json) 
- 
+HCC_Data4_JSON <- fromJSON("../data/hcc_data.json") 
+#print(HCC_Data3_JSON) 
 
-insert_url = "https://cloud1.griddb.com/trial1602/griddb/v2/gs_clustertrial1602/dbs/r_blog/containers/HCC_Data/rows/"  
-r <- PUT(insert_url, add_headers("Content-Type" = "application/json; charset=UTF-8" ) , config = authenticate("pratik", "pratik"), encode = "json" , body=hcc_data_JSON ) 
+insert_url = "https://cloud1.griddb.com/trial1602/griddb/v2/gs_clustertrial1602/dbs/r_blog/containers/HCC_Data4/rows/"  
+r <- PUT(insert_url, 
+         add_headers("Content-Type" = "application/json; charset=UTF-8" ) , 
+		 config = authenticate("pratik", "pratik"), 
+		 encode = "json" ,
+		 body=HCC_Data4_JSON ) 
 
-#To check if all rows have been inserted
-print(str(json.loads(r.text)['count']) + ' rows have been registered in the container HCC_Data.') 
-
+#To check if all rows have been inserted  
+#print(r) 
+print("Rows inserted")	  
 #Fetch(Query) Only 2 columns are of interest, Alive( i.e. years alive after HCC) and Packs_of_cigarets_per_year (Packs of cigarretes per year) . 
-my_sql_query1 = '(f"""SELECT Packs_of_cigarets_per_year, Alive FROM HCC_Data """) '
+my_sql_query1 = "SELECT Packs_of_cigarets_per_year, Alive FROM HCC_Data4 " 
 
 #To retieve data from a GridDB container, the GridDB Web API Query URL must be suffied with "/sql" 
 my_query_url = "https://cloud1.griddb.com/trial1602/griddb/v2/gs_clustertrial1602/dbs/r_blog/sql"
 
-#Construct the request body 
-query_request_body = '[ {"type" : "sql-select", "stmt" : "SELECT Packs_of_cigarets_per_year, Alive FROM HCC_Data" }]' 
+#Construct the request body  
+
+query_request_body = ' [{"type":"sql-select" , "stmt":"SELECT Packs_of_cigarets_per_year, Alive FROM HCC_Data4" }] '
 
 # print(query_request_body ) 
 #Invoke the GridDB WebAPI request 
